660c64851e8c928ba40ef7b0c2649a49b86862c1
Nick Jurista <nick@inexact-scientist.com>
2017-11-11 18:00:26 -0500

    Adding logs

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'logger'
 
 # set up all the stuff we need to load (like models/controllers, etc)
 require_relative 'bootstrap'
 
 class App < Sinatra::Base
+  ::Logger.class_eval { alias :write :'<<' }
+  access_log = "#{Dir.pwd}/logs/access.log"
+  access_logger = ::Logger.new( access_log )
+
+  error_log = "#{Dir.pwd}/logs/error.log"
+  error_logger = ::File.new( error_log, "a+" )
+  error_logger.sync = true
+
+  configure do
+    use ::Rack::CommonLogger, access_logger
+  end
+
+  before {
+    env[ "rack.errors" ] = error_logger
+  }
+
   get '/' do
     IndexController.instance.randomize
   end
