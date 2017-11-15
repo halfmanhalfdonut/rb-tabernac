e69b61924acc4865e64c739db05503f5d470fa2b
Nick Jurista <nick@inexact-scientist.com>
2017-11-14 20:54:43 -0500

    change statement to IN

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -22,24 +22,13 @@ module Tabernac
       def get_verses( bible, book_id, chapter_id, start_verse_id, end_verse_id )
         verses = []
 
-        stmt = ""
         if end_verse_id
           range = (start_verse_id .. end_verse_id).to_a
-          range.each_with_index do |verse_id, index|
-            if index == 0
-              stmt = "( verse_id = #{verse_id} or "
-            elsif index == range.length - 1
-              stmt += "verse_id = #{verse_id} )"
-            else
-              stmt += "verse_id = #{verse_id} or "
-            end
-          end
         else
           range = [ start_verse_id ]
-          stmt = "verse_id = #{start_verse_id}"
         end
 
-        rows = @db.execute( "select \"text\" from #{bible} where book_id = :book_id and chapter_id = :chapter_id and #{stmt}",
+        rows = @db.execute( "select \"text\" from #{bible} where book_id = :book_id and chapter_id = :chapter_id and verse_id in (#{range.join(', ')})",
           :book_id => book_id,
           :chapter_id => chapter_id
         )
